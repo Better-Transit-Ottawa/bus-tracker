42a2f7243c7f847825744cc35a7ba57cae0b2a3d
Ajay <dev@ajay.app>
2025-12-12 14:34:30 -0500

    Fix error handling

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,7 @@ import { fetchRealtime } from "./utils/fetchRealtime.ts";
 const interval = 60 * 1000;
 
 setInterval(() => {
-    try {
-        fetchRealtime();
-    } catch (e) {
-        console.error("Error fetching real-time data:", e);
-    }
+    fetchRealtime().catch((e) => console.error("Error fetching real-time data:", e));
 }, interval);
 
 fetchRealtime();

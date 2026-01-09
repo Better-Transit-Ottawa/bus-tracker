23ac06414b17f7fae0b790cd614c781b9b5e3ef3
Eric Warren <ericwarren99@hotmail.com>
2026-01-09 09:58:29 -0500

    added gtfs schedule fetching from oc zip link with scheduling for 1am each day

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { fetchRealtime } from "./utils/fetchRealtime.ts";
+import { fetchGtfs } from "./utils/fetchGtfs.ts";
 import Fastify, { type FastifyInstance } from "fastify";
 import { createBusCountEndpoint } from "./endpoints/busCount.ts";
 import { createBlockDetailsEndpoint } from "./endpoints/blockDetails.ts";
@@ -8,8 +9,11 @@ import { createListVehiclesEndpoint } from "./endpoints/listVehicles.ts";
 import { config } from "./utils/config.ts";
 import { createRouteDetailsEndpoint } from "./endpoints/routeDetails.ts";
 import { createListRoutesEndpoint } from "./endpoints/listRoutes.ts";
+import schedule from 'node-schedule';
+import fs from 'fs';
 
-const interval = 60 * 1000;
+const schedulePath = 'schedule/schedule.zip';
+
 const server: FastifyInstance = Fastify({
     logger: {
         level: "error",
@@ -19,10 +23,20 @@ const server: FastifyInstance = Fastify({
     }
 });
 
-setInterval(() => {
-    fetchRealtime().catch((e) => console.error("Error fetching real-time data:", e));
-}, interval);
-fetchRealtime();
+// Schedule GTFS data fetch at 1 AM daily
+schedule.scheduleJob('0 0 1 * * *', () => 
+    fetchGtfs().catch(e => console.error("Error fetching GTFS data:", e))
+)
+if (!fs.existsSync(schedulePath)) {
+    fetchGtfs().catch(e => console.error("Error fetching GTFS data:", e));
+}
+
+/*
+// Schedule real-time data fetch every minute
+schedule.scheduleJob('* * * * *', () => 
+    fetchRealtime().catch(e => console.error("Error fetching real-time data:", e))
+);
+fetchRealtime();*/
 
 createBusCountEndpoint(server);
 createBlockDetailsEndpoint(server);
